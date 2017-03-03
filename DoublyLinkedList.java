d539c1112a1bd87e04f464446720d01bdd223585
Akeem Shane <awshane420@gmail.com>
2017-03-03 01:04:02 -0500

    properties added to DoublyLinkedList class

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -2,6 +2,18 @@ package H3;
 
 public class DoublyLinkedList {
 	
+	private Node head;
+	private int size;
 	
+	public Node getHead(){
+		return this.head;
+	}
+	public void setHead(Node head){
+		this.head = head;
+	}
+	
+	public int getSize(){
+		return this.size;
+	}
 
 }
